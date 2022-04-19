cc418d84b7a67ea36966f7ce0e04c8c2dbcc9f27
Catalinbrm <73463644+Catalinbrm@users.noreply.github.com>
2022-04-19 20:35:45 +0300

    Composite

---
+++ Composite/Composite/BrandSystemComponent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite
+{
+    abstract class BrandSystemComponent
+    {
+        public string Name { get; }
+
+        public BrandSystemComponent(string name)
+        {
+            Name = name;
+        }
+
+        public abstract void Display(int level = 0);
+    }
+}
---
+++ Composite/Composite/Folder.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite
+{
+    class Folder : BrandSystemComponent
+    {
+        private List<BrandSystemComponent> _components = new List<BrandSystemComponent>();
+
+        public Folder(string name) : base(name)
+        {
+
+        }
+
+        public override void Display(int level = 0)
+        {
+            string spaces = new string(' ', level);
+
+            Console.WriteLine(spaces + "└ " + Name);
+            foreach (var component in _components)
+                component.Display(level + 1);
+        }
+
+        public void AddComponent(BrandSystemComponent component)
+        {
+            _components.Add(component);
+        }
+
+        public void RemoveComponent(BrandSystemComponent component)
+        {
+            _components.Remove(component);
+        }
+    }
+}
