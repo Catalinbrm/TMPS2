2480e797cb69d91837f60c43ac0e8780c0e9281d
Catalinbrm <73463644+Catalinbrm@users.noreply.github.com>
2022-04-19 20:37:02 +0300

    Add files via upload

---
+++ Composite/Composite/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Composite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Folder BRAND = new Folder("BRAND");
+
+            Brand clothes_brand1 = new Brand("BOSS");
+            Brand clothes_brand2 = new Brand("LEVI'S");
+            Brand clothes_brand3 = new Brand("GUESS");
+            Folder CLOTHES = new Folder("CLOTHES");
+
+            Brand shoes_brand1 = new Brand("NIKE");
+            Brand shoes_brand2 = new Brand("PUMA");
+            Brand shoes_brand3 = new Brand("ADIDAS");
+            Folder SHOES = new Folder("SHOES");
+
+            CLOTHES.AddComponent(clothes_brand1);
+            CLOTHES.AddComponent(clothes_brand2);
+            CLOTHES.AddComponent(clothes_brand3);
+            BRAND.AddComponent(CLOTHES);
+
+            SHOES.AddComponent(shoes_brand1);
+            SHOES.AddComponent(shoes_brand2);
+            SHOES.AddComponent(shoes_brand3);
+            BRAND.AddComponent(SHOES);
+
+            BRAND.Display();
+
+            
+        }
+    }
+}
