fb42eccf65f1f0f7112a867888efbdba2deea102
Catalinbrm <73463644+Catalinbrm@users.noreply.github.com>
2022-04-19 20:37:30 +0300

    Proxy

---
+++ Proxy/Proxy/IShoesRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy
+{
+    interface IShoesRepository
+    {
+        Shoes GetShoesById(int id);
+        void InsertShoes(Shoes shoes);
+        void RemoveShoesById(int id);
+    }
+}
